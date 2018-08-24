ba0089d0cfd0ce62d1bf2ee12142b8388b325855
Gregor Cox <gregorcox@gmail.com>
2018-08-24 11:11:34 +0100

    initial commit

---
+++ testing/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -0,0 +1,38 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function(){
+    assert.equal(true, true)
+  })
+
+  it('should be able to add 2 numbers', function(){
+    calculator.previousTotal = 1;
+    calculator.add(4);
+    assert.equal(5, calculator.runningTotal);
+  })
+
+  it('should be able to subtract 2 numbers', function(){
+    calculator.previousTotal = 7;
+    calculator.subtract(4);
+    assert.equal(3, calculator.runningTotal);
+  })
+
+  it('should be able to multiply 2 numbers', function (){
+    calculator.previousTotal = 3;
+    calculator.multiply(5);
+    assert.equal(15, calculator.runningTotal);
+  })
+
+  it('should be able to divide 2 numbers', function (){
+    calculator.previousTotal = 21;
+    calculator.divide(7);
+    assert.equal(3, calculator.runningTotal);
+  })
+
+});
---
+++ testing/ruby/test.rb
@@ -0,0 +1,14 @@
+# cash = {"John" => 10, "Zsolt" => 9, "Tony" => 8, "Jarrod" => 7}
+#
+# def total_cash()
+#   total = 0
+#   for
+
+
+numbers = [9, 21, 34, 43, 65, 1, 90]
+
+def sort_numbers(number_list)
+  number_list.sort!
+end
+
+p sort_numbers(numbers)
