58b61502860ff18c5d5924156e85cd4b3acc2303
Gregor Cox <gregorcox@gmail.com>
2018-08-24 12:49:43 +0100

    two UI integration tests passing

--- testing/js_calculator_start_point/tests/integration/tests.js
+++ testing/js_calculator_start_point/tests/integration/tests.js
@@ -16,4 +16,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  it('should have working operator buttons', function (){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('5')
+  })
+
 });
