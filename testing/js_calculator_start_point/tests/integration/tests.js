0dac0f9066ce26d761f105e3797df669868920d2
Gregor Cox <gregorcox@gmail.com>
2018-08-27 09:08:42 +0100

    can chain multiple functions together

--- testing/js_calculator_start_point/tests/integration/tests.js
+++ testing/js_calculator_start_point/tests/integration/tests.js
@@ -25,4 +25,16 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('5')
   })
 
+  it('should be able to chain multiple functions together', function () {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('8')
+  })
+
+
 });
