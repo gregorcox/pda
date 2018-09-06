2cb6b1d3b84559a318c28ec281629b7eeed628d0
Gregor Cox <gregorcox@gmail.com>
2018-09-06 12:04:09 +0100

    static ruby testing done:

---
+++ testing/ruby/testing_task_1.rb
@@ -0,0 +1,49 @@
+### Testing task 2 code:
+
+# Carry out dynamic testing on the code below.
+# Correct the errors below that you spotted in task 1.
+
+require_relative('card.rb')
+class CardGame
+
+
+  def checkforAce(card)
+    if card.value = 1
+      return true
+    else
+      return false
+    end
+  end
+
+ # method should be names check_for_ave as convention
+ # should be == rather than =
+
+
+  dif highest_card(card1 card2)
+  if card1.value > card2.value
+    return card.name
+  else
+    card2
+  end
+end
+end
+
+# code should be indented properly
+# def instead of 'dif'
+# should use a comma between card1 and card2 arguments
+# card does not have an attribute of .name
+# should have 'return card2' instead of just card2
+# extra unneccesary end
+
+def self.cards_total(cards)
+  total
+  for card in cards
+    total += card.value
+    return "You have a total of" + total
+  end
+end
+
+# total should be initialized, should be total = 0
+# the string return should be after the end of the for loop
+#
+# class has no 'end'
